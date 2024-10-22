684a8fb2b15bb34472d5a4c66bcb295c92488b02
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-10-22 21:40:53 +0200

    configured typeOrm pg

---
+++ typeOrmConfig.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+
+export const typeOrmConfig : TypeOrmModuleOptions ={
+    type : 'postgres',
+    host : 'localhost',
+    port : 5432,
+    username : 'postgres',
+    password : '',
+    database : '',
+    entities : [],
+    synchronize : false
+
+
+}
