cbab1fb53df50f70ce2b864a8659f5e97fdd4a0b
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-10-23 15:19:36 +0200

    enabled the cookie-parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './components/users/users.module';
+import { JwtMiddleware } from './components/services.ts/jwtMiddleware';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(JwtMiddleware).forRoutes('*')
+  }
+}
