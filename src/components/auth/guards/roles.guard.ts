8bd712176903cac4c060b10b191cf8ac91bb84f7
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-10-29 09:02:22 +0200

    condition to return true if no role is specified in a routr

--- src/components/auth/guards/roles.guard.ts
+++ src/components/auth/guards/roles.guard.ts
@@ -14,6 +14,9 @@ export class RolesGuard implements CanActivate {
       context.getHandler(),
       context.getClass(),
     ])
+    if (!requiredRoles){
+      return true
+    }
     const user = context.switchToHttp().getRequest().user;
     const hasRequiredRole = requiredRoles.some((role)=>user.role===role)
     return hasRequiredRole;
