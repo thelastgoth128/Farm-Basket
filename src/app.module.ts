3039f51f8f195d825e9829fbcab5a8e16637c2b5
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-06 22:42:16 +0200

    setup a global typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,34 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './components/auth/auth.module';
 import { ProductsModule } from './components/products/products.module';
 import { ShopModule } from './components/shop/shop.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './components/users/entities/user.entity';
+import { Product } from './components/products/entities/product.entity';
+import { Shop } from './components/shop/entities/shop.entity';
 
 @Module({
-  imports: [UsersModule,JwtModule,AuthModule, ProductsModule, ShopModule],
+  imports: [UsersModule,JwtModule,AuthModule, ProductsModule, ShopModule,
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath: ".env"
+    }),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory:async (configService : ConfigService)=>({
+        type:'postgres',
+        host:configService.get<string>('SUPABASE_HOST'),
+        username:configService.get<string>('SUPABASE_USER'),
+        password:configService.get<string>('SUPABASE_PASS'),
+        port:configService.get<number>('SUPABASE_PORT'),
+        database:configService.get<string>('SUPABASE_NAME'),
+        autoLoadEntities:true,
+        entities:[Users,Product,Shop],
+        synchronize: false
+      }),
+      inject:[ConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
