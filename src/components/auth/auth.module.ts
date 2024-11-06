73de71c1fdc6010f78843d60182dd4fae3dd09ae
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-06 22:46:35 +0200

    added a reset-password method

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { AuthService } from "./auth.service";
 import { APP_GUARD } from "@nestjs/core";
 import { AuthGuard } from "./guards/authGuard";
 import { MailService } from "../services.ts/mail.service";
+import { UsersService } from "../users/users.service";
 
 
 @Module({
