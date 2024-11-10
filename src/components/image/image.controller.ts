a3ab5d3d3f11cea35f0bf85ef55e34e97395ec18
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-10 17:29:53 +0200

    setup the image post end route

--- src/components/image/image.controller.ts
+++ src/components/image/image.controller.ts
@@ -1,9 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ImageService } from './image.service';
-import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { CloudinaryService } from "../services.ts/cloudinary.service";
+import { FileInterceptor } from "@nestjs/platform-express";
+import * as path from "path";
+import * as fs from 'fs'
+import * as sharp from "sharp";
+import { ImageService } from "./image.service";
+import { Public } from "../auth/guards/public";
 
-@Controller('image')
-export class ImageController {
- 
+@Public()
+@Controller('images')
+export class ImageController{
+    constructor(
+        private readonly cloudinaryService : CloudinaryService,
+        private readonly imageService : ImageService
+    ){}
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('image'))
+    async uploadFile(@UploadedFile() file: Express.Multer.File,@Body('shopid') shopid:number) {
+      try{
+        const result = await this.cloudinaryService.uploadImage(file.path)
+        await this.imageService.saveImageUrl(shopid,result.url)
+        return result
+      }catch(error) {
+        console.error('Error uploading image: ', error);
+        throw new Error('Failed to upload image')
+      } 
+    }
 }
+
+
