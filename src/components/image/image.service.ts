6d09f76232abdb8083029272127251476443b161
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-10 17:31:40 +0200

    created the save url to database for shop image

--- src/components/image/image.service.ts
+++ src/components/image/image.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { Shop } from "../shop/entities/shop.entity";
+import { Repository } from "typeorm";
+
 
 @Injectable()
 export class ImageService {
- 
+  constructor(
+    @InjectRepository(Shop)
+    private readonly shoprep : Repository<Shop>,
+){}
+
+async saveImageUrl(shopid: number, imageUrl : string): Promise<void> {
+    const shop = await this.shoprep.findOne({where : {shopid}})
+    if (!shop) {
+       throw new HttpException('Shop not found', HttpStatus.NOT_FOUND) 
+    }
+    shop.image = imageUrl
+    await this.shoprep.save(shop)
+}
 }
