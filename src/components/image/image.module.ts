b9667aadbe41e872b76c14827cc22fc16fef918e
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-10 17:30:18 +0200

    exported the imageservice

--- src/components/image/image.module.ts
+++ src/components/image/image.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
+import { ShopModule } from '../shop/shop.module';
+import { CloudinaryService } from '../services.ts/cloudinary.service';
 
 @Module({
+  imports:[ShopModule],
   controllers: [ImageController],
-  providers: [ImageService],
+  providers: [ImageService,CloudinaryService],
+  exports:[ImageService]
 })
 export class ImageModule {}
