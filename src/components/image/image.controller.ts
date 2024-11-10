50728041e201b50a40cfde1e6082786570fd85c8
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-10 09:24:27 +0200

    removed boiler methods

--- src/components/image/image.controller.ts
+++ src/components/image/image.controller.ts
@@ -5,30 +5,5 @@ import { UpdateImageDto } from './dto/update-image.dto';
 
 @Controller('image')
 export class ImageController {
-  constructor(private readonly imageService: ImageService) {}
-
-  @Post()
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imageService.create(createImageDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.imageService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.imageService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateImageDto: UpdateImageDto) {
-    return this.imageService.update(+id, updateImageDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.imageService.remove(+id);
-  }
+ 
 }
