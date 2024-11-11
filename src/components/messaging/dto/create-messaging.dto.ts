f25b4a85f0b1bdb4f264c0557725a7b23b2f98cc
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-11 22:44:58 +0200

    created a create messaging the dto

--- src/components/messaging/dto/create-messaging.dto.ts
+++ src/components/messaging/dto/create-messaging.dto.ts
@@ -1,1 +1,14 @@
-export class CreateMessagingDto {}
+import { IsInt, IsNotEmpty, IsString } from "class-validator";
+
+
+export class CreateMessagingDto {
+    @IsInt()
+    userid: number
+
+    @IsInt()
+    inboxid : number
+
+    @IsString()
+    @IsNotEmpty()
+    messages : string
+}
