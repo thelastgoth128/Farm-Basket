521eef1679763572db3483258e898fcc6310a7e8
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-11 22:21:48 +0200

    imported the enties in the module

--- src/components/messaging/messaging.module.ts
+++ src/components/messaging/messaging.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MessagingService } from './messaging.service';
 import { MessagingController } from './messaging.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Inbox } from './entities/inbox.entity';
+import { Messages } from './entities/messaging.entity';
+import { InboxParticipants } from './entities/inbox_participants.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports : [
+    TypeOrmModule.forFeature([Inbox,Messages,InboxParticipants]),UsersModule
+  ],
   controllers: [MessagingController],
   providers: [MessagingService],
 })
