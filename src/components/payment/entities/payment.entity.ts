fc140915984e7461e50cd70703d41c8eca7b6884
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-13 01:31:41 +0200

    created the payment entity

--- src/components/payment/entities/payment.entity.ts
+++ src/components/payment/entities/payment.entity.ts
@@ -1,1 +1,44 @@
-export class Payment {}
+import { PaymentStatus } from "src/components/enums/payment.enum";
+import { Users } from "src/components/users/entities/user.entity";
+import { Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export class Payments {
+    @PrimaryGeneratedColumn()
+    id : number
+
+    @ManyToOne(()=>Users,user=>user.userid)
+    @JoinColumn({ name : 'userid'})
+    userid : number
+    
+    @Column()
+    Amount : number
+
+    @Column()
+    reference : string
+
+    @Column({
+        type:'enum',
+        enum : PaymentStatus,
+        default : PaymentStatus.PENDING
+    })
+    status : PaymentStatus
+
+    @Column()
+    mobile : number
+
+    @Column()
+    currency : string
+
+    @Column({
+        type:'timestamp',
+        default:'CURRENT_TIMESTAMPT'
+
+    })
+    created_at : Date
+
+    @Column({
+        type : 'timestamp',
+        default:'CURRENT_TIMESTAMP'
+    })
+    updated_at : Date
+}
