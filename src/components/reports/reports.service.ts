0ac482204acf5b5c7404c4cfcd4e1a7852d59f56
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-12 15:21:38 +0200

    created a create,get all and get one function for reports

--- src/components/reports/reports.service.ts
+++ src/components/reports/reports.service.ts
@@ -4,16 +4,50 @@ import { UpdateReportDto } from './dto/update-report.dto';
 
 @Injectable()
 export class ReportsService {
-  create(createReportDto: CreateReportDto) {
-    return 'This action adds a new report';
+  constructor(
+    @InjectRepository(Reports)
+    private readonly reportRep : Repository<Reports>,
+    @InjectRepository(Users)
+    private readonly userrep : Repository<Users>,
+    @InjectRepository(Products)
+    private productrep : Repository<Products>
+  ){}
+  async create(createReportDto: CreateReportDto,@Req() req: Request) {
+    try {
+
+      const{ product, reporter, ...ReportData } = createReportDto
+      const userid = req.user?.userid
+      const user = await this.userrep.findOne({where : {userid}})
+      const productid = await this.productrep.findOne({where: {productid : product}})
+    
+      if (!user) {
+        throw new NotFoundException('User not found')
+      }
+      if (!productid) {
+        throw new NotFoundException('Product not found')
+      }
+      const report = new Reports()
+      report.reporter = user
+      report.product = productid
+      await this.reportRep.save(report)
+      
+      response.status(201).json ({
+        message : "Reported Successfully"
+      })
+    }catch (error){
+      response.status(500).json({
+        message : "Internal Server Error"
+      })
+    }
+      
   }
 
-  findAll() {
-    return `This action returns all reports`;
+  async findAll() {
+    return await this.reportRep.find() 
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} report`;
+  async findOne(id: number) {
+    return await this.reportRep.findOne({where : {id}})
   }
 
   update(id: number, updateReportDto: UpdateReportDto) {
