af8b99769c57117dbbac617db671a7afecdcdca7
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-12 14:48:38 +0200

    added fields for both the created and update dto

--- src/components/reports/dto/create-report.dto.ts
+++ src/components/reports/dto/create-report.dto.ts
@@ -1,1 +1,34 @@
-export class CreateReportDto {}
+import { IsDateString, IsInt, IsString } from "class-validator";
+
+export class CreateReportDto {
+@IsInt()
+reporter : number
+
+@IsInt()
+product : number
+
+@IsString()
+reason : number
+
+@IsString()
+details : number
+
+@IsString()
+status : number
+
+@IsDateString()
+reported_date : Date
+
+@IsString()
+resolution_details : string
+
+@IsDateString()
+reolution_date : Date
+
+@IsInt()
+ban_duartion : number
+
+@IsString()
+punishment : string
+
+}
