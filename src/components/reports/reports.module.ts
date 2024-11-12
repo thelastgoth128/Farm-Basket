1b0210fd02a92d27e4e9208bb830bc1de611f2b4
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-12 15:20:54 +0200

    imported usermodule and products module

--- src/components/reports/reports.module.ts
+++ src/components/reports/reports.module.ts
@@ -3,10 +3,12 @@ import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reports } from './entities/report.entity';
+import { UsersModule } from '../users/users.module';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
   imports :[
-    TypeOrmModule.forFeature([Reports])
+    TypeOrmModule.forFeature([Reports]),UsersModule,ProductsModule
   ],
   controllers: [ReportsController],
   providers: [ReportsService],
