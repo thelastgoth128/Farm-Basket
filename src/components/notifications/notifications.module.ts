5a7bfe3f94f41c7aade5c5f670fa30e5d2c7886f
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-14 01:02:01 +0200

    imported entity with typeorm

---
+++ src/components/notifications/notifications.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { NotificationsService } from './notifications.service';
+import { NotificationsController } from './notifications.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notifications } from './entities/notification.entity';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports : [
+    TypeOrmModule.forFeature([Notifications]),UsersModule
+  ],
+  controllers: [NotificationsController],
+  providers: [NotificationsService],
+  exports : [TypeOrmModule,NotificationsService]
+})
+export class NotificationsModule {}
