723164351c519ba6c87720596e5ed8f4bde0b32a
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-13 16:53:55 +0200

    imported entity with typeorm

--- src/components/payment/payment.module.ts
+++ src/components/payment/payment.module.ts
@@ -3,10 +3,12 @@ import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payments } from './entities/payment.entity';
+import { UsersModule } from '../users/users.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([Payments])
+    TypeOrmModule.forFeature([Payments]),UsersModule,HttpModule
   ],
   controllers: [PaymentController],
   providers: [PaymentService],
