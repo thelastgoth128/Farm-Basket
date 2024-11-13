bf00c053bcd64ecb38e3b3ecda5ecb9fc3c73e3f
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-13 20:38:50 +0200

    added a productid for product to buy

--- src/components/payment/dto/create-payment.dto.ts
+++ src/components/payment/dto/create-payment.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsInt, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreatePaymentDto {
     @IsNotEmpty()
@@ -24,4 +24,7 @@ export class CreatePaymentDto {
     @IsOptional()
     @IsString()
     name: string
+
+    @IsInt()
+    productid : number
 }
