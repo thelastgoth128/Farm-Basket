2d7d2b51650452472a566b7fcc9f7554ae395c5b
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-13 16:55:51 +0200

    implemented the make payment.verify initialize methods

--- src/components/payment/payment.service.ts
+++ src/components/payment/payment.service.ts
@@ -1,26 +1,200 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HttpService } from '@nestjs/axios';
+import { Users } from '../users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Payments } from './entities/payment.entity';
+import { v4 as uuidv4 } from 'uuid'
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class PaymentService {
-  create(createPaymentDto: CreatePaymentDto) {
-    return 'This action adds a new payment';
+  constructor(
+    @InjectRepository(Payments)
+    private readonly paymentrep : Repository<Payments>,
+    @InjectRepository(Users)
+    private readonly usersrep : Repository<Users>,
+    private httpService : HttpService
+
+  ){}
+
+  private readonly operatorIds = {
+    '8': '27494cb5-ba9e-437f-a114-4e7a7686bcca',
+    '9': '20be6c20-adeb-4b5b-a7ba-0769820df4fb',
   }
 
-  findAll() {
-    return `This action returns all payment`;
+  private getMobileMoneyOperatorIds(mobile : string) {
+    const prefix = mobile.charAt(0)
+    const refId = this.operatorIds[prefix]
+    if (!refId) {
+      throw new HttpException('Unsupported mobile number', HttpStatus.BAD_REQUEST)
+    }
+    return refId
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} payment`;
+  private transactionId():string {
+    return uuidv4()
   }
 
-  update(id: number, updatePaymentDto: UpdatePaymentDto) {
-    return `This action updates a #${id} payment`;
+
+  async processPayment(createPaymentDto : CreatePaymentDto): Promise<any> {
+    const { amount,mobile } = createPaymentDto
+    const mobileMoneyOperatorId = this.getMobileMoneyOperatorIds(mobile)
+    const charge_id = this.transactionId()
+
+    createPaymentDto.tx_ref = this.transactionId()
+
+    const options = {
+      headers: {
+        accept : 'application/json',
+        'content-type' : 'application/json',
+        Authorization : `Bearer ${process.env.PAYCHANGU_SECRET_KEY}`
+      }
+    }
+
+    try {
+      const response = await firstValueFrom (
+        this.httpService.post(
+          'https://api.paychangu.com/mobile-money/payments/initialize',
+          {
+            ...createPaymentDto,
+            mobile,
+            mobile_money_operator_ref_id: mobileMoneyOperatorId,
+            amount : amount,
+            charge_id: charge_id
+          },
+          options
+        )
+      )
+      const data = response.data
+
+      if ( data.status === 'success') {
+        return {
+          statusCode: 200,
+          message: 'Payment initiated successfully',
+          data: data.data,
+        }
+      }else {
+        throw new Error(data.message || 'Payment initiation failed.')
+      }
+    }catch (error) {
+      console.error(
+        'Error processing payment:',
+        error.response?.data || error.message,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} payment`;
+
+  async getPaymentStatus(tx_ref: string): Promise<any> {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.get(
+          `https://api.paychangu.com/payment/status/${tx_ref}`
+        )
+      )
+      const data = response.data
+      if (data.status === 'success') {
+        return {
+          statusCode: 200,
+          message: 'Payment status retrieved successfully.',
+          data: data.data
+        }
+      }else {
+        throw new Error(data.message)
+      }
+    }catch (error) {
+      console.error(
+        'Error retrieving payment status:',
+         error.message
+      )
+      throw new Error(
+        error.response?.data?.message 
+      )
+    }  
+  }
+
+  async verifyPayment(tx_ref : string): Promise<any> {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.get(
+          `https://api.paychangu.com/verify-payment/${tx_ref}`,
+          {
+            headers: {
+              Accept: 'application/json',
+              Authorization: `Bearer ${process.env.PAYCHANGU_SECRET_KEY}`,
+          }
+        }
+        )
+      )
+      const data = response.data
+
+      if (data.status === 'success') {
+        const details = data.data
+        
+        return {
+          statusCode : 200,
+          message : 'Payment verified successfully',
+          data: details
+        }
+      }else {
+        throw new HttpException(
+          data.message || 'Payment verification failed.',
+          HttpStatus.BAD_REQUEST,
+        )
+      }
+    }catch (error) {
+      console.error(
+        'Error verifying payment:',
+        error.response.data || error.message,
+      )
+      throw new HttpException(
+        error.resposne.data.message || 'An error occured while verifying payment.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      )
+    }
+  }
+
+
+  async initiatePayout(mobile:string, amount : string): Promise<any> {
+    const mobileMoneyOperatorId = this.getMobileMoneyOperatorIds(mobile)
+    const charge_id = this.transactionId()
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(
+          'https://api.paychangu.com/mobile-money/payments/initialize',
+          {
+            mobile,
+            mobile_money_operator_ref_id: mobileMoneyOperatorId,
+            amount,
+            charge_id
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.PAYCHANGU_SECRET_KEY}`,
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            }
+          }
+        )
+      )
+
+      
+      if (response.data.status === 'success') {
+        return {
+          statusCode: 200,
+          message: 'Payout initiated successfully.',
+          data: response.data.data,
+        };
+      } else {
+        throw new HttpException('Failed to initiate mobile money payout.', HttpStatus.BAD_REQUEST);
+      }
+    } catch (error) {
+      console.error('Error initiating payout:', error.response?.data || error.message);
+      throw new HttpException('An error occurred while processing payout.', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
