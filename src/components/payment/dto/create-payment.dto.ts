a1a1bd0e9c06cdf140b671657bfb4a158921f123
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-13 16:56:05 +0200

    created dto's

--- src/components/payment/dto/create-payment.dto.ts
+++ src/components/payment/dto/create-payment.dto.ts
@@ -1,1 +1,27 @@
-export class CreatePaymentDto {}
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreatePaymentDto {
+    @IsNotEmpty()
+    @IsString()
+    amount : string
+
+    @IsNotEmpty()
+    @IsString()
+    currency :string
+
+    @IsNotEmpty()
+    @IsString()
+    email : string
+
+    @IsNotEmpty()
+    @IsString()
+    tx_ref: string
+
+    @IsOptional()
+    @IsString()
+    mobile : string
+
+    @IsOptional()
+    @IsString()
+    name: string
+}
