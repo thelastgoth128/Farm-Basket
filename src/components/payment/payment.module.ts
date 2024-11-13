bed780126028b517ec61293bd09d098ed9063d17
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-13 20:38:07 +0200

    exported typeorm

--- src/components/payment/payment.module.ts
+++ src/components/payment/payment.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payments } from './entities/payment.entity';
 import { UsersModule } from '../users/users.module';
 import { HttpModule } from '@nestjs/axios';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([Payments]),UsersModule,HttpModule
+    TypeOrmModule.forFeature([Payments]),UsersModule,HttpModule,ProductsModule
   ],
   controllers: [PaymentController],
   providers: [PaymentService],
+  exports:[TypeOrmModule]
 })
 export class PaymentModule {}
