a6c46a6ccb64187771656b148ee6f1ef86c906de
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-22 23:44:23 +0200

    imported entities and exported typeorm

--- src/components/cart/cart.module.ts
+++ src/components/cart/cart.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart, CartItem } from './entities/cart.entity';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { ProductsModule } from '../products/products.module';
+import { ShopModule } from '../shop/shop.module';
 
 @Module({
-  providers: [CartService],
-  controllers: [CartController]
+  imports:[
+    TypeOrmModule.forFeature([Cart,CartItem]),UsersModule,ProductsModule,ShopModule
+  ],
+  providers: [CartService,UsersService],
+  controllers: [CartController],
+  exports: [TypeOrmModule]
 })
 export class CartModule {}
