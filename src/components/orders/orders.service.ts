e8f7ad3984ced8730af51d4b81d87009d14d20c9
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-25 22:04:25 +0200

    added find and delete methods

--- src/components/orders/orders.service.ts
+++ src/components/orders/orders.service.ts
@@ -1,26 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderrep: Repository<Order>,
+  ){}
+
+  async findAll() {
+    return await this.orderrep.find() 
   }
 
-  findAll() {
-    return `This action returns all orders`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: number) {
+    return  await this.orderrep.findOne({where: {id}})
   }
 
   update(id: number, updateOrderDto: UpdateOrderDto) {
     return `This action updates a #${id} order`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  async remove(id: number) {
+    const order = await this.orderrep.findOne({where: {id}})
+    if (!order) {
+      throw new NotFoundException('Order not found')
+    }
+    return await this.orderrep.remove(order)
   }
 }
