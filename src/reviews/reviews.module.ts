3fbfcc542b6f7bf13027a4eb375d91f59eb8a233
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-27 04:59:49 +0200

    setup typeorm for reviews module

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
+import { UsersModule } from 'src/components/users/users.module';
+import { ProductsModule } from 'src/components/products/products.module';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Review]),UsersModule,ProductsModule
+  ],
   providers: [ReviewsService],
-  controllers: [ReviewsController]
+  controllers: [ReviewsController],
+  exports:[TypeOrmModule],
 })
 export class ReviewsModule {}
